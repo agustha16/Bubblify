a6ad295f23138dac59a1639cfc07268e663e07bd
agustbrokkur <ahagbgaha@gmail.com>
2019-03-07 16:50:08 +0000

    Added service for bundle.

--- template/src/services/bubbleService.js
+++ template/src/services/bubbleService.js
@@ -24,7 +24,35 @@ const getBubbleById = (id) => {
     });
 }
 
+const getAllBundles = () => {
+    return fetch("http://localhost:3500/api/bundles").then(resp => {
+        if (resp.ok) {
+            return resp.json();
+        }
+    }).then(data => {
+        if (!data) {
+            return [];
+        }
+        return data;
+    });
+}
+
+const getBundleById = (id) => {
+    return fetch("http://localhost:3500/api/bundles/" + id).then(resp => {
+        if (resp.ok) {
+            return resp.json();
+        }
+    }).then(data => {
+        if (!data) {
+            return {};
+        }
+        return data;
+    });
+}
+
 export default {
   getAllBubbles,
-  getBubbleById
+  getBubbleById,
+  getAllBundles,
+  getBundleById
 };
